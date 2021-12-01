209e19c447b12d1b267e08dd97ea86b6014947ed
John Gutierrez <gutierrezjohn2000@gmail.com>
2021-12-01 13:13:38 -0600

    Cahce class

--- cachesimulator.py
+++ cachesimulator.py
@@ -9,14 +9,25 @@
 
 import sys
 
-from .menu import simulator_menu
+# from menu import simulator_menu
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     filename = args[0].strip()
 
+    if filename != "input.txt":
+        print("Invalid filename. Expected: 'input.txt'")
+        print("quitting...")
+        quit()
+    
+    # 1. Initialize the physical memory
+    # + byte-addressable
     print("*** Welcome to the cache simulator ***")
-    init = input("initialize the RAM:\n")
+    init = input("initialize the RAM:\n").split(" ")  # expected command is 'init-ram 0x# 0x#'
+    ramStart, ramEnd = int(init[1], 0), int(init[2], 0)
+    cacheSize = ramEnd - ramStart + 1
+    print(cacheSize)
 
     # simulator_menu()
+    # [set][line][char]
