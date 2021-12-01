ea676a88bbd38e97be8a463ba99923a6eb38c307
cbherring <66192540+cbherring@users.noreply.github.com>
2021-12-01 13:33:30 -0600

    Gig'em

--- configurecache.py
+++ configurecache.py
@@ -4,17 +4,17 @@ global cache, C, B, E, S, s, b, t
 
 def configure_cache():
     print("configure the cache:")
-    C = input("cache size: ")
-    B = input("data block size: ")
-    E = input("associativity: ")
-    replacementPolicy = input("replacement policy: ")
-    writeHit = input("write hit policy: ")
-    writeMiss = input("write miss policy: ")
+    C = int(input("cache size: "))
+    B = int(input("data block size: "))
+    E = int(input("associativity: "))
+    replacementPolicy = int(input("replacement policy: "))
+    writeHit = int(input("write hit policy: "))
+    writeMiss = int(input("write miss policy: "))
     print("cache successfully configured!")
     S = int(C/(B*E))
     s = math.log2(S)
     b = math.log2(B)
-    t = m-(s+b)
+    t = int(m-(s+b))
     cache.build_cache(S, E, B)
 
 def build_cache(S, E, B):
